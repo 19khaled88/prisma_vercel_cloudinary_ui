4754b4bfe44a317d9342d6f3872e1a52a243c624
19khaled88 <kkhaled88hasan@gmail.com>
2023-11-18 17:50:24 +0600

    change public url

--- src/app/(dashboardLayout)/dashboard/authorList/page.tsx
+++ src/app/(dashboardLayout)/dashboard/authorList/page.tsx
@@ -4,9 +4,35 @@ import React from 'react'
 const page = () => {
     const authors = fetch(`${process.env.API_LINK}/author/allAuthor`).then(res => res.json()).then(data => { return data })
     console.log(authors)
-  return (
-    <div>page</div>
-  )
+    return (
+        <div><table className="table-auto">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Image</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                    <td>Malcolm Lockyer</td>
+                    <td>1961</td>
+                </tr>
+                <tr>
+                    <td>Witchy Woman</td>
+                    <td>The Eagles</td>
+                    <td>1972</td>
+                </tr>
+                <tr>
+                    <td>Shining Star</td>
+                    <td>Earth, Wind, and Fire</td>
+                    <td>1975</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+    )
 }
 
 export default page
